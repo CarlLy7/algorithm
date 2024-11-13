785225a483d1aaf6e0e4364f95bcffe5776eb0bb
luanyingqi <yingqi.luan@skp-beijing.com>
2024-11-13 11:08:02 +0800

    双指针解决链表问题

---
+++ src/day20241113Solution.java
@@ -0,0 +1,177 @@
+import java.util.PriorityQueue;
+
+/**
+ * @author: carl
+ * @date: 2024/11/13
+ */
+
+public class day20241113Solution {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    //21
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode p = dummy;
+        ListNode p1 = l1, p2 = l2;
+        while (p1 != null && p2 != null) {
+            if (p1.val <= p2.val) {
+                p.next = p1;
+                p1 = p1.next;
+            } else {
+                p.next = p2;
+                p2 = p2.next;
+            }
+            p = p.next;
+        }
+        if (p1 != null) {
+            p.next = p1;
+        }
+        if (p2 != null) {
+            p.next = p2;
+        }
+        return dummy.next;
+    }
+
+    //86
+    public ListNode partition(ListNode head, int x) {
+        ListNode dummy1 = new ListNode(-1);
+        ListNode dummy2 = new ListNode(-1);
+        ListNode p1 = dummy1;
+        ListNode p2 = dummy2;
+        ListNode p = head;
+        while (p != null) {
+            if (p.val < x) {
+                p1.next = p;
+                p1 = p1.next;
+            } else {
+                p2.next = p;
+                p2 = p2.next;
+            }
+            ListNode temp = p.next;
+            p.next = null;
+            p = temp;
+        }
+        p1.next = dummy2.next;
+        return dummy1.next;
+    }
+
+    //23
+    public ListNode mergeKList(ListNode[] lists) {
+        if (lists.length == 0) {
+            return null;
+        }
+        PriorityQueue<ListNode> queue = new PriorityQueue<>(lists.length, (a, b) -> a.val - b.val);
+        for (ListNode node : lists) {
+            queue.offer(node);
+        }
+        ListNode dummy = new ListNode(-1);
+        ListNode p = dummy;
+        while (!queue.isEmpty()) {
+            ListNode node = queue.poll();
+            p.next = node;
+            p = p.next;
+            if (node.next != null) {
+                queue.offer(node.next);
+            }
+        }
+        return dummy.next;
+    }
+
+    //19
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(-1);
+        ListNode node = findLastNNode(dummy, n + 1);
+        node.next = node.next.next;
+        return dummy.next;
+    }
+
+    /**
+     * 寻找倒数第N个节点
+     *
+     * @param head
+     * @param n
+     * @return
+     */
+    private ListNode findLastNNode(ListNode head, int n) {
+        ListNode slow = head;
+        ListNode fast = head;
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
+        }
+        while (fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+
+    //876
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    //142
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            // 只要有环直接退出循环
+            if (slow == fast) {
+                break;
+            }
+        }
+        // 无环
+        if (fast == null || fast.next == null) {
+            return null;
+        }
+        slow = head;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+
+    //160
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode a = headA;
+        ListNode b = headB;
+        while (a != b) {
+            if (a == null) {
+                a = headB;
+            } else {
+                a = a.next;
+            }
+            if (b == null) {
+                b = headA;
+            } else {
+                b = b.next;
+            }
+        }
+        return a;
+    }
+
+}
