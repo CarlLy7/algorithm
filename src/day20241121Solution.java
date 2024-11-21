cd411175d03722a9d220afa030f8950745a0c5a6
luanyingqi <yingqi.luan@skp-beijing.com>
2024-11-21 17:59:09 +0800

    【强化练习】 二分搜索算法经典习题-部分

--- src/day20241121Solution.java
+++ src/day20241121Solution.java
@@ -160,17 +160,87 @@ public class day20241121Solution {
         return res;
     }
 
-    private int leftBound(int[] arr, int x) {
+    private int leftBound(int[] arr, int target) {
         int left = 0, right = arr.length;
         while (left < right) {
             int mid = left + (right - left) / 2;
-            if (arr[mid] >= x) {
+            if (arr[mid] >= target) {
                 right = mid;
-            } else if (arr[mid] < x) {
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            }
+        }
+        if (left >= arr.length || arr[left] != target) {
+            return -1;
+        }
+        return left;
+    }
+
+    //35
+    public int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] >= target) {
+                right = mid;
+            } else {
                 left = mid + 1;
             }
         }
         return left;
     }
 
+    //162
+    public int findPeakElement(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[mid + 1]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    //852
+    // 0 10 5 2   1
+    // 0 2 1 0   1
+    public int peakIndexInMountainArray(int[] nums) {
+        int left = 0, right = nums.length;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[mid + 1]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    public int search(int[] nums, int target) {
+        int leftIndex = leftBound(nums, target);
+        int rightIndex = rightBound(nums, target);
+        return rightIndex - leftIndex + 1;
+    }
+
+    private int rightBound(int[] nums, int target) {
+        int left = 0, right = nums.length-1;
+        while (left < right) {
+            int mid=left+(right-left)/2;
+            if (nums[mid]==target){
+                left=mid+1;
+            }else if (nums[mid]>target){
+                right=mid;
+            }else{
+                left=mid+1;
+            }
+        }
+        if (right<0 || nums[right]!=target){
+            return -1;
+        }
+        return right;
+    }
 }
