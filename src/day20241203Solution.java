e460fd687f4b40b040aecd80b97d980a29c4862c
luanyingqi <yingqi.luan@skp-beijing.com>
2024-12-03 16:27:14 +0800

    背包类型算法

---
+++ src/day20241203Solution.java
@@ -0,0 +1,58 @@
+/**
+ * @author: luanyingqi
+ * @date: 2024/12/3
+ */
+
+public class day20241203Solution {
+    //416
+    public boolean canPartition(int[] nums) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        if (sum % 2 != 0) {
+            return false;
+        }
+        sum = sum / 2;
+        int n = nums.length;
+        boolean[][] dp = new boolean[n + 1][sum + 1];
+        // base case
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = true;
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= sum; j++) {
+                // 放不进去了
+                if (j - nums[i - 1] < 0) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]];
+                }
+            }
+        }
+        return dp[n][sum];
+    }
+
+    //322
+    public int change(int amount, int[] coins) {
+        int n = coins.length;
+        int[][] dp = new int[n + 1][amount + 1];
+        // base case
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= amount; j++) {
+                // 只能够不选择这个硬币
+                if (j - coins[i - 1] < 0) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    // 选择或者不选
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i - 1]];
+                }
+            }
+        }
+        return dp[n][amount];
+    }
+
+}
