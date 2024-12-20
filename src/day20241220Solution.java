67a8a29843f0a2e36f9e99a01fa72ed52013c48d
luanyingqi <yingqi.luan@skp-beijing.com>
2024-12-20 15:32:21 +0800

    【强化练习】 回溯算法经典习题II

--- src/day20241220Solution.java
+++ src/day20241220Solution.java
@@ -8,9 +8,22 @@ import java.util.Map;
 
 public class day20241220Solution {
 
+    //121
+    public int solution(int[] prices){
+        int n=prices.length;
+        int[][] dp=new int[n][2];
+        // base case
+        dp[0][0]=0;
+        dp[0][1]=-prices[0];
+        for (int i = 1; i < n; i++) {
+            //不持有股票
+            dp[i][0]=Math.max(dp[i-1][1]+prices[i],dp[i-1][0]);
+            dp[i][1]=Math.max(dp[i-1][1],-prices[i]);
+        }
+        return dp[n-1][0];
+    }
+
     //473
-
-
     public boolean makesquare(int[] matchsticks) {
         // 是否能等分为4组
         return canPartitionKSubsets(matchsticks,4);
