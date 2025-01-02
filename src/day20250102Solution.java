b2e6deceadb6c0dcbf4e566d51c6ddb8a03d447e
luanyingqi <yingqi.luan@skp-beijing.com>
2025-01-02 10:33:52 +0800

    旅游省钱大法：加权最短路径

---
+++ src/day20250102Solution.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * @author: carl
+ * @date: 2025/1/2
+ */
+
+public class day20250102Solution {
+
+    //787
+    int src;
+    int dst;
+    int[][] memo;
+    HashMap<Integer, List<int[]>> map;
+
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int K) {
+        this.src = src;
+        this.dst = dst;
+        map = new HashMap<>();
+        K++;
+        memo = new int[n][K + 1];
+        for (int[] ints : memo) {
+            Arrays.fill(ints, -888);
+        }
+        for (int[] flight : flights) {
+            int from = flight[0];
+            int to = flight[1];
+            int price = flight[2];
+            map.putIfAbsent(to, new ArrayList<>());
+            map.get(to).add(new int[]{from, price});
+        }
+        return dp(dst, K);
+    }
+
+    private int dp(int s, int k) {
+        if (s == src) {
+            return 0;
+        }
+        if (k == 0) {
+            return -1;
+        }
+        if (memo[s][k] != -888) {
+            return memo[s][k];
+        }
+        int res = Integer.MAX_VALUE;
+        for (int[] in : map.get(s)) {
+            int from = in[0];
+            int price = in[1];
+            int subProblem = dp(from, k - 1);
+            if (subProblem != -1) {
+                res = Math.min(res, subProblem + price);
+            }
+        }
+        memo[s][k] = res == Integer.MAX_VALUE ? -1 : res;
+        return memo[s][k];
+    }
+
+}
