898f0ae3ac33d7efa4c58b84e70564f5627c0325
luanyingqi <yingqi.luan@skp-beijing.com>
2025-01-21 09:35:06 +0800

    滑动窗口算法

--- src/day20250121Solution.java
+++ src/day20250121Solution.java
@@ -66,37 +66,37 @@ public class day20250121Solution {
     // }
 
     // 567
-    // public boolean checkInclusion(String t, String s) {
-    // Map<Character, Integer> window = new HashMap<>();
-    // Map<Character, Integer> need = new HashMap<>();
-    // for (char c : t.toCharArray()) {
-    // need.put(c, need.getOrDefault(c, 0) + 1);
-    // }
-    // int valid = 0;
-    // int left = 0, right = 0;
-    // while (right < s.length()) {
-    // char c = s.charAt(right);
-    // right++;
-    // if (need.containsKey(c)) {
-    // window.put(c, window.getOrDefault(c, 0) + 1);
-    // if (window.get(c).equals(need.get(c))) {
-    // valid++;
-    // }
-    // }
-    // while (right - left >= t.length()) {
-    // if (valid == need.size()) {
-    // return true;
-    // }
-    // char d = s.charAt(left);
-    // left++;
-    // if (need.containsKey(d)) {
-    // if (need.get(d).equals(window.get(d))) {
-    // valid--;
-    // }
-    // window.put(d, window.get(d) - 1);
-    // }
-    // }
-    // }
-    // return false;
-    // }
+//    public boolean checkInclusion(String t, String s) {
+//        Map<Character, Integer> window = new HashMap<>();
+//        Map<Character, Integer> need = new HashMap<>();
+//        for (char c : t.toCharArray()) {
+//            need.put(c, need.getOrDefault(c, 0) + 1);
+//        }
+//        int valid = 0;
+//        int left = 0, right = 0;
+//        while (right < s.length()) {
+//            char c = s.charAt(right);
+//            right++;
+//            if (need.containsKey(c)) {
+//                window.put(c, window.getOrDefault(c, 0) + 1);
+//                if (window.get(c).equals(need.get(c))) {
+//                    valid++;
+//                }
+//            }
+//            while (right - left >= t.length()) {
+//                if (valid == need.size()) {
+//                    return true;
+//                }
+//                char d = s.charAt(left);
+//                left++;
+//                if (need.containsKey(d)) {
+//                    if (need.get(d).equals(window.get(d))) {
+//                        valid--;
+//                    }
+//                    window.put(d, window.get(d) - 1);
+//                }
+//            }
+//        }
+//        return false;
+//    }
 }
