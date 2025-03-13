8308c3d7fb766e3a1a9b8adcba3e4e04660f4bad
luanyingqi <yingqi.luan@skp-beijing.com>
2025-03-13 13:45:28 +0800

    二叉树习题【练习】 用遍历思维解题I 199

--- src/day20250313Solution.java
+++ src/day20250313Solution.java
@@ -1,5 +1,7 @@
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * @author: carl
@@ -24,49 +26,97 @@ public class day20250313Solution {
     }
 
     // 257
-//    LinkedList<String> res = new LinkedList<>();
-//    LinkedList<String> path = new LinkedList<>();
-//
-//    public List<String> binaryTreePaths(TreeNode root) {
-//        traverse(root);
-//        return res;
-//    }
-//
-//    private void traverse(TreeNode root) {
-//        if (root == null) {
-//            return;
-//        }
-//        // 到达叶子节点
-//        if (root.left == null && root.right == null) {
-//            path.addLast(root.val + "");
-//            res.addLast(String.join("->", path));
-//            path.removeLast();
-//            return;
-//        }
-//        path.addLast(root.val + "");
-//        traverse(root.left);
-//        traverse(root.right);
-//        path.removeLast();
-//    }
+    // LinkedList<String> res = new LinkedList<>();
+    // LinkedList<String> path = new LinkedList<>();
+    //
+    // public List<String> binaryTreePaths(TreeNode root) {
+    // traverse(root);
+    // return res;
+    // }
+    //
+    // private void traverse(TreeNode root) {
+    // if (root == null) {
+    // return;
+    // }
+    // // 到达叶子节点
+    // if (root.left == null && root.right == null) {
+    // path.addLast(root.val + "");
+    // res.addLast(String.join("->", path));
+    // path.removeLast();
+    // return;
+    // }
+    // path.addLast(root.val + "");
+    // traverse(root.left);
+    // traverse(root.right);
+    // path.removeLast();
+    // }
 
-    //129
-    StringBuilder path=new StringBuilder();
-    int res=0;
-    public int sumNumbers(TreeNode root){
+    // 129
+    // StringBuilder path=new StringBuilder();
+    // int res=0;
+    // public int sumNumbers(TreeNode root){
+    // traverse(root);
+    // return res;
+    // }
+    //
+    // private void traverse(TreeNode root) {
+    // if (root==null){
+    // return;
+    // }
+    // path.append(root.val);
+    // if (root.left==null && root.right==null){
+    // res+=Integer.parseInt(path.toString());
+    // }
+    // traverse(root.left);
+    // traverse(root.right);
+    // path.deleteCharAt(path.length()-1);
+    // }
+
+    // 199
+    // public List<Integer> rightSideView(TreeNode root) {
+    // List<Integer> res = new LinkedList<>();
+    // if (root == null) {
+    // return res;
+    // }
+    // Queue<TreeNode> queue = new LinkedList<>();
+    // queue.offer(root);
+    // while (!queue.isEmpty()) {
+    // int size = queue.size();
+    // TreeNode lastNode = queue.peek();
+    // for (int i = 0; i < size; i++) {
+    // TreeNode cur = queue.poll();
+    // if (cur.right != null) {
+    // queue.offer(cur.right);
+    // }
+    // if (cur.left != null) {
+    // queue.offer(cur.left);
+    // }
+    // }
+    // res.add(lastNode.val);
+    // }
+    // return res;
+    // }
+
+    // dfs算法实现
+    List<Integer> res = new ArrayList<>();
+    int depth = 0;
+
+    public List<Integer> rightSideView(TreeNode root) {
         traverse(root);
         return res;
     }
 
     private void traverse(TreeNode root) {
-        if (root==null){
+        if (root == null) {
             return;
         }
-        path.append(root.val);
-        if (root.left==null && root.right==null){
-            res+=Integer.parseInt(path.toString());
+        depth++;
+        // 如果res中的节点数小于深度说明此时这个节点就是右侧视图应该放进去
+        if (res.size() < depth) {
+            res.add(root.val);
         }
-        traverse(root.left);
         traverse(root.right);
-        path.deleteCharAt(path.length()-1);
+        traverse(root.left);
+        depth--;
     }
 }
